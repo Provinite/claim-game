c5575c05906023791e2baafb1c58771e8d731e56
Collin Driscoll <provinite@gmail.com>
2021-05-31 18:55:15 -0500

    enable ssl

--- src/db/queryBuilder.ts
+++ src/db/queryBuilder.ts
@@ -1,10 +1,13 @@
 import knex from "knex";
 
 const dbUrl = process.env.cc_claim_game_db_url || process.env.DATABASE_URL;
-console.log(dbUrl);
+
+if (!dbUrl) {
+  throw new Error("No database configuration set, exiting.");
+}
 
 export const queryBuilder = knex({
   client: "pg",
-  connection: dbUrl,
+  connection: { connectionString: dbUrl, ssl: true },
   searchPath: ["public"],
 });
