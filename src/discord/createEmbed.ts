31f1c4727575a84bd3cd4feb2afd24e99a91a56d
Collin Driscoll <provinite@gmail.com>
2021-06-01 00:41:17 -0500

    update default embed url to claim game git

--- src/discord/createEmbed.ts
+++ src/discord/createEmbed.ts
@@ -6,6 +6,6 @@ export function createEmbed() {
     .setAuthor(
       "Prov",
       "https://avatars.githubusercontent.com/u/13734094?s=400&u=09327218ed4a22862c6d1ce9e68116f7a26d5ada&v=4",
-      "https://github.com/provinite"
+      "https://github.com/provinite/claim-game"
     );
 }
