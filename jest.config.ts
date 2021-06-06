3161610c66f40ebfa37445d72b3c8c40ff6c203d
Collin Driscoll <provinite@gmail.com>
2021-06-06 00:57:30 -0500

    add jest-junit

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,10 @@ const config: InitialOptionsTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/src/test/global_hooks.ts"],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "./reports/junit" }],
+  ],
 };
 
 export default config;
