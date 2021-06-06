cd24100a9f49191fe8f723e158c4d13759cf29e8
Prov <Provinite@users.noreply.github.com>
2021-06-06 01:22:17 -0500

    v1.1.3 - Add code coverage
    Add jest-junit
    Add coverage

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,13 @@ const config: InitialOptionsTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/src/test/global_hooks.ts"],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "./reports/junit" }],
+  ],
+  coverageDirectory: "./coverage",
+  collectCoverage: true,
+  coverageProvider: "v8",
 };
 
 export default config;
